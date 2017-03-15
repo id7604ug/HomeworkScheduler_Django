25b5064d90fc1891d159efd86b0df036e81bc657
Anthony Flowers <id7604ug@go.minneapolis.edu>
2017-03-15 08:53:48 -0500

    Edited base html template and used more css styles

--- assignment_scheduler/urls.py
+++ assignment_scheduler/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='main_menu'),
-    url(r'^$', views.assignment_list, name='assignment_list'),
+    url(r'^assignment_list/$', views.assignment_list, name='assignment_list'),
     url(r'^assignment/new/$', views.add_assignment, name='add_assignment'),
+    # url(r'^$', views.view_all_assignments, name='view_all_assignments'),
+    # url(r'^delete_assignment/$', views.delete_assignment, name='delete_assignment'),
+    url(r'^assignment/(?P<id>\d+)/$', views.assignment_read, name='assignment_read'),
+    url(r'^delete_assignment/(?P<id>\d+)/$', views.assignment_delete, name='assignment_delete'),
+    url(r'^check_due_assignments/$', views.check_due_assignments, name='check_due_assignments')
+
 ]
--- assignment_scheduler/views.py
+++ assignment_scheduler/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import ScheduleItem
 from .forms import AddAssignmentForm
+from django.utils import timezone
 
-# Create your views here.
+def index(request):
+    assignments_list = ScheduleItem.objects.all()
+    assignments_due_today = assignments_list.filter(due_date=timezone.now().date())
+    assignments_due_tomorrow = assignments_list.filter(due_date=(timezone.now() + timezone.timedelta(days=1)).date())
+    assignments_past_due = assignments_list.filter(due_date__lt=timezone.now().date())
+    print(len(assignments_past_due))
+    return render(request, 'assignment_scheduler/index.html', {'due_today':assignments_due_today,'due_tomorrow':assignments_due_tomorrow,'past_due':assignments_past_due})
 
 def assignment_list(request):
     assignments = ScheduleItem.objects.all()
     return render(request, 'assignment_scheduler/assignment_list.html', {'assignments': assignments})
-
-def index(request):
-    return render(request, 'assignment_scheduler/index.html', {})
 
 def add_assignment(request):
     if request.method =="POST":
@@ -17,7 +21,19 @@ def add_assignment(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-            return redirect('assignment_detail', pk=post.pk)
+            assignment = ScheduleItem.objects.get(id=post.pk)
+            return render(request, 'assignment_scheduler/read_one_assignment.html', {'assignment': assignment})
     else:
         form = AddAssignmentForm()
     return render(request, 'assignment_scheduler/add_assignment.html', {'form': form})
+
+def assignment_read(request, id):
+    assignment = ScheduleItem.objects.get(id=id)
+    return render(request, 'assignment_scheduler/read_one_assignment.html', {'assignment':assignment})
+
+def assignment_delete(request, id):
+    assignments = ScheduleItem.objects.get(id=id).delete()
+    return redirect('/assignment_list/', {'assignments': assignments})
+
+def check_due_assignments(request):
+    return render(request, 'assignment_scheduler/check_due_assignments.html', {})
